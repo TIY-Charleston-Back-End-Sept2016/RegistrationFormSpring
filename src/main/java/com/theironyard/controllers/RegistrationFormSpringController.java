2a9fd3104dff48c783e8b08c00e51ac6593030a6
oakes <zsoakes@gmail.com>
2016-10-26 10:42:21 -0400

    Add entire backend

--- src/main/java/com/theironyard/controllers/RegistrationFormSpringController.java
+++ src/main/java/com/theironyard/controllers/RegistrationFormSpringController.java
@@ -1,7 +1,10 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.User;
+import com.theironyard.services.UserRepository;
 import org.h2.tools.Server;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -12,6 +15,9 @@ import java.sql.SQLException;
  */
 @RestController
 public class RegistrationFormSpringController {
+    @Autowired
+    UserRepository users;
+
     Server h2Server;
 
     @PostConstruct
@@ -22,5 +28,25 @@ public class RegistrationFormSpringController {
     @PreDestroy
     public void destroy() {
         h2Server.stop();
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.POST)
+    public User addUser(@RequestBody User user) {
+        return users.save(user);
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.GET)
+    public Iterable<User> getUsers() {
+        return users.findAll();
+    }
+
+    @RequestMapping(path = "/user", method = RequestMethod.PUT)
+    public User editUser(@RequestBody User user) {
+        return users.save(user);
+    }
+
+    @RequestMapping(path = "/user/{id}", method = RequestMethod.DELETE)
+    public void deleteUser(@PathVariable("id") int id) {
+        users.delete(id);
     }
 }
